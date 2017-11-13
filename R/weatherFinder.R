41d4358f8c7329cc55a59b7d09ee7ce6665f043c
Jake Klein <jakeklein@Jakes-MacBook-Pro.local>
2017-11-13 13:09:14 -0500

    added weather scraping function, weatherFinder.r

---
+++ R/weatherFinder.R
@@ -0,0 +1,20 @@
+loader(data.table)
+
+weatherFinder = function(station, gamedate){
+  url = paste0('https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?',
+    'station=', station,
+    '&data=all&year1=', year(gamedate),
+    '&month1=', month(gamedate),
+    '&day1=', mday(gamedate),
+    '&year2=', year(gamedate),
+    '&month2=', month(gamedate),
+    '&day2=', mday(gamedate),
+    '&tz=', paste0(tstrsplit(tz,'/')[1],"%2F",tstrsplit(tz,'/')[2]),
+    '&format=onlycomma&latlon=no&direct=no&report_type=1&report_type=2');
+  vars = c('station', 'valid', 'tmpf', 'relh', 'alti');
+  colnames = c('station', 'time', 'tempf', 'humidity', 'pressure');
+  data = fread(url, sep = ",", header = TRUE,verbose=FALSE)[tmpf!='M'][,..vars];
+  colnames(data) = colnames;
+  return(data[,time_dif:=abs(as.POSIXct(time)-gamedate)][which.min(time_dif)])
+}
+
